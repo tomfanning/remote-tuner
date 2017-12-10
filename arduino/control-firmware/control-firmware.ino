e2d2d2fa8f9d26f3988b5f3e2a3f8a3e846d4836
Thomas <tf@elizabeths-air.localnet>
2017-12-10 21:42:27 +0000

    Tidy up a bit

--- arduino/control-firmware/control-firmware.ino
+++ arduino/control-firmware/control-firmware.ino
@@ -31,14 +31,24 @@ void setup() {
   delay(500);
   cap1Serial.println("0");
 
-  cap1Serial.listen();
+  queryCaps(0);
 
+  queryCaps(1);
+  
+  Serial.println("setup ended");
+}
+
+// query the board at the specified serial port index for the caps installed and the capacitances attainable
+void queryCaps(int serialPort) {
+  
+  serialListen(serialPort);
   bool go=true;
   String line1,line2;
   while (go) {
-    cap1Serial.println("q");
+    
+    serialPrintln(serialPort, "q");
     char arr[255];
-    int numChars = getLine(1000, arr);
+    int numChars = getLine(serialPort, 1000, arr);
     if (numChars != 0) {
       // we have a string
       for (int i=0;i<numChars;i++) {
@@ -47,7 +57,7 @@ void setup() {
       }
     }
     if (!go) {
-      numChars = getLine(1000, arr);
+      numChars = getLine(serialPort, 1000, arr);
       for (int i=0;i<numChars;i++) {
         line2 += arr[i];
       }
@@ -57,20 +67,22 @@ void setup() {
   Serial.println(line1);
   Serial.println(line2);
 
-  Serial.println("setup ended");
 }
 
 // Read a line from a serial port, within the specified timeout (ms), into the referenced array.
 // If a line (terminated in \n) has not been received within the timeout, return 0, else, return the number of chars on the line.
-int getLine(int timeout, char arr[]) {
+int getLine(int serialPort, int timeout, char arr[]) {
   long started;
   int cur=0;
   while (true) {
     started = millis();
-    cap1Serial.println("q");
+    //cap1Serial.println("q");
+    serialPrintln(serialPort, "q");
     while (millis() < started + timeout) {
-      if (cap1Serial.available()) {
-        int b = cap1Serial.read();
+      //if (cap1Serial.available()) {
+      if (serialAvailable(serialPort)) {
+        //int b = cap1Serial.read();
+        int b = serialRead(serialPort);
         if (b == '\n') {
           return cur;
         }
@@ -80,6 +92,46 @@ int getLine(int timeout, char arr[]) {
       }
     }
   }
+}
+
+// Activate the specified serial port
+void serialListen(int index) {
+  if (index == 0){
+    cap1Serial.listen();
+  } else if (index == 1) {
+    cap2Serial.listen();
+  }
+}
+
+// Read a byte from the specified serial port
+int serialRead(int index) {
+  if (index == 0) {
+    return cap1Serial.read();
+  } else if (index == 1) {
+    return cap2Serial.read();
+  }
+
+  return 0;
+}
+
+// Print a line to the specified serial port
+void serialPrintln(int index, String s) {
+  if (index == 0) {
+    cap1Serial.println(s);
+  } else if (index == 1) {
+    cap2Serial.println(s);
+  }
+}
+
+// Return whether there is data available at the specified serial port
+bool serialAvailable(int index) {
+  if (index == 0) {
+    return cap1Serial.available();
+  } else if (index == 1) {
+    return cap2Serial.available();
+  }
+
+  return false;
 }
 
 int parseInto(String buf, int arr[]) {
